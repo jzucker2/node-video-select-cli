8b994c8168e66ac3e7005c6a12e666db31761973
Jordan Zucker <jordan.zucker@gmail.com>
2019-09-22 19:20:21 -0700

    Saving initial node script, got it all working

---
+++ index.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+
+const commander = require('commander');
+const Denon = require('denon-client');
+
+const DENON_IP = '10.0.1.101'
+const VERSION = '0.1.0'
+
+const program = new commander.Command();
+program.version(VERSION);
+
+program
+  .option('-d, --debug', 'output extra debugging')
+  .option('--ip', 'ip of Denon receiver', DENON_IP)
+  .option('-vsc, --video-select-cancel', 'Cancel video select mode')
+  .option('-gvs, --get-video-select-mode', 'Get video select status')
+  .option('-vs, --video-select-mode <type>', 'set video select mode');
+
+program.parse(process.argv);
+
+console.log(`ip: ${program.ip}`);
+console.log(`video-select-cancel: ${program.videoSelectCancel}`);
+console.log(`get-video-select-mode: ${program.getVideoSelectMode}`);
+console.log(`video-select-mode: ${program.videoSelectMode}`);
+
+/**
+ * Denon is now an object containing DenonClient and Options.
+ * Use the DenonClient to send requests. Use the Options to define the data.
+ */
+const denonClient = new Denon.DenonClient(program.ip);
+
+// Connecting
+denonClient
+  .connect()
+  .then(() => {
+    if (program.getVideoSelectMode) {
+      console.log('Retrieving video select mode setting.')
+      return denonClient.getVideoSelectMode();
+    } else if (program.videoSelectMode) {
+      console.log(`Setting videoSelectMode to ${program.videoSelectMode}`);
+      return denonClient.setVideoSelectMode(program.videoSelectMode);
+    }
+  })
+  .then((result) => {
+    console.log(`getVideoSelectMode ==> ${result}`);
+    return denonClient.disconnect();
+  })
+  .catch((error) => {
+    // Oh noez.
+    console.error(error);
+    return denonClient.disconnect();
+  });
