dad5dfee9343ce4e8cbced52454fa8185a0892e1
Jordan Zucker <jordanz@pubnub.com>
2021-10-17 19:14:32 -0700

    Add a dangling comma

--- src/constants.js
+++ src/constants.js
@@ -3,5 +3,5 @@ import process from 'process';
 export default {
     DENON_IP_ADDRESS: process.env.DENON_IP_ADDRESS || "10.0.1.101",
     HOST: process.env.HOST || "0.0.0.0",
-    PORT: process.env.PORT || 3131
+    PORT: process.env.PORT || 3131,
 }
